32cdbcd0277b7a3322a66bbbdbdaace9be4a7921
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-08-16 23:16:27 +0900

    feat(search): 地図上に現在地と検索範囲を表示

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,12 +2,14 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:latlong2/latlong.dart';
 
 // Project imports:
 import 'package:yorimichi_radar/widgets/radar/radar_circle.dart';
+import 'package:yorimichi_radar/widgets/search/currentLocationContainer.dart';
 
 part '../widgets/search/search_page_bottom_sheet.dart';
 part '../widgets/search/search_page_search_button.dart';
--- lib/widgets/search/search_page_map.dart
+++ lib/widgets/search/search_page_map.dart
@@ -4,9 +4,14 @@ class SearchPageMap extends HookConsumerWidget {
   const SearchPageMap({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final LatLng currentLocation = LatLng(
+      35.02238185907218,
+      135.96166673359087,
+    );
+
     return FlutterMap(
       options: MapOptions(
-        initialCenter: LatLng(34.98513206363541, 136.0144106884364),
+        initialCenter: currentLocation,
         initialZoom: 14,
         maxZoom: 16,
         minZoom: 8,
@@ -18,13 +23,22 @@ class SearchPageMap extends HookConsumerWidget {
         ),
         CircleLayer(
           circles: [
-            CircleMarker(
-              point: LatLng(34.98513206363541, 136.0144106884364), //中心の座標
-              radius: 1000, //m単位で書く
-              useRadiusInMeter: true,
-              color: Colors.transparent,
-              borderStrokeWidth: 4,
+            SearchCircleMarker(
+              point: currentLocation,
+              radius: 1000,
               borderColor: Theme.of(context).colorScheme.primary,
+            ),
+          ],
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+              point: currentLocation,
+              child: Currentlocationcontainer(
+                diameter: 32,
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                lineColor: Theme.of(context).colorScheme.surface,
+              ),
             ),
           ],
         ),
@@ -36,4 +50,15 @@ class SearchPageMap extends HookConsumerWidget {
       ],
     );
   }
+}
+
+class SearchCircleMarker extends CircleMarker {
+  const SearchCircleMarker({
+    required super.point,
+    required super.radius,
+    required super.borderColor,
+    super.useRadiusInMeter = true,
+    super.color = Colors.transparent,
+    super.borderStrokeWidth = 4,
+  });
 }
