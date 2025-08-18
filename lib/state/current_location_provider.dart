ad49f004131fd3ab5bd7b66b1c52c671270a7303
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-08-19 00:15:40 +0900

    refactor: UI/UXの改善と設定ページの追加
    主な変更点:
    - MainPageのAppBarタイトルを動的に変更。
    - IndexedStackを使用してページの切り替えを実装。
    - 新しく設定ページを追加し、ダークモードや履歴削除などの機能を提供。
    - 各ウィジェットの軽微なスタイル調整。
    - 不要なimport文を削除。

--- lib/state/current_location_provider.dart
+++ lib/state/current_location_provider.dart
@@ -2,9 +2,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:latlong2/latlong.dart';
 
-// Project imports:
-import 'package:yorimichi_radar/widgets/search/currentLocationContainer.dart';
-
 final currentLocationProvider = Provider<LatLng>((ref) {
   return LatLng(35.02238185907218, 135.96166673359087);
 });
